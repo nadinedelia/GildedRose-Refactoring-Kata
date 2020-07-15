776848f180d2971eb2ade9926ffd662d92988320
nadinedelia <nadine.delia@yahoo.com>
2020-07-15 18:32:14 +0100

    printing stuff out

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,19 @@
 require 'gilded_rose'
+require 'item'
 
 describe GildedRose do
 
   describe "#update_quality" do
+
     it "does not change the name" do
-      items = [Item.new("pasta", 0, 0)]
-      GildedRose.new(items).update_quality()
+      p items = [Item.new("pasta", 2, 5)]
+      p GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "pasta"
     end
+  end
+
+  it "changes the Sell-in value by 1" do 
+
   end
 
 end
