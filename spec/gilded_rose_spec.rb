f79065f2d3d752f34a41b202e3e45f1d9335f966
nadinedelia <nadine.delia@yahoo.com>
2020-07-15 18:36:48 +0100

    code changes sell in value

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,14 +6,16 @@ describe GildedRose do
   describe "#update_quality" do
 
     it "does not change the name" do
-      p items = [Item.new("pasta", 2, 5)]
-      p GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "pasta"
+      items = [Item.new("Pasta", 2, 5)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "Pasta"
     end
   end
 
   it "changes the Sell-in value by 1" do 
-
+      items = [Item.new("Pizza", 10, 12)]
+      GildedRose.new(items).update_quality()
+      expect { GildedRose.new(items).update_quality() }.to change { items[0].sell_in }.by(-1)
   end
 
 end
