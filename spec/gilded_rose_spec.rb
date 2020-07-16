0c714a9ebc00344d448e8dbd20d2f2ea5502ede4
nadinedelia <nadine.delia@yahoo.com>
2020-07-16 11:50:18 +0100

    renaming items that they match texttest

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,26 +6,24 @@ describe GildedRose do
   describe "#update_quality" do
 
     it "does not change the name" do
-      items = [Item.new("Pasta", 10, 10)]
+      items = [Item.new("Elixir of the Mongoose", 10, 10)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "Pasta"
+      expect(items[0].name).to eq "Elixir of the Mongoose"
     end
-  end
 
   it "when still in date, change quality by 1" do 
-      items = [Item.new("Eggs Benedict", 10, 10)]
+      items = [Item.new("Elixir of the Mongoose", 10, 10)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq(9)
       expect(items[0].quality).to eq(9)
   end
 
   it "checks that if the sell by date has passed, quality degrades twice as fast" do
-    items = [Item.new("Eggs Benedict", 0, 10)] 
+    items = [Item.new("Elixir of the Mongoose", 0, 10)] 
     GildedRose.new(items).update_quality()
     expect(items[0].quality).to eq(8)
   end
 
-  # "Aged Brie" & Backstage Passes actually increases in Quality the older it gets
 
   it "brie increases quality with age" do
     items = [Item.new("Aged Brie", 10, 10)] 
@@ -39,7 +37,6 @@ describe GildedRose do
     expect(items[0].quality).to eq(12)
   end
 
-# The Quality of an item is never more than 50
 
   it 'quality is never more than 50' do
       items = [Item.new("Aged Brie", 5, 50)] 
@@ -48,7 +45,6 @@ describe GildedRose do
     end
 
 
-    # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
   it 'Sulfuras never have to be sold or decrease quality' do
     items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 10)] 
     GildedRose.new(items).update_quality()
@@ -57,3 +53,4 @@ describe GildedRose do
   end
 
   end
+end
