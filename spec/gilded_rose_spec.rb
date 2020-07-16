12453f875481811f2d113c842ff87b6b6137d715
nadinedelia <nadine.delia@yahoo.com>
2020-07-16 11:44:34 +0100

    changed numbers for readability

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,17 +6,17 @@ describe GildedRose do
   describe "#update_quality" do
 
     it "does not change the name" do
-      items = [Item.new("Pasta", 2, 5)]
+      items = [Item.new("Pasta", 10, 10)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "Pasta"
     end
   end
 
   it "when still in date, change quality by 1" do 
-      items = [Item.new("Eggs Benedict", 25, 12)]
+      items = [Item.new("Eggs Benedict", 10, 10)]
       GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq(24)
-      expect(items[0].quality).to eq(11)
+      expect(items[0].sell_in).to eq(9)
+      expect(items[0].quality).to eq(9)
   end
 
   it "checks that if the sell by date has passed, quality degrades twice as fast" do
@@ -34,9 +34,9 @@ describe GildedRose do
   end
 
   it "backstage passes increase quality with age" do
-    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 2, 3)] 
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 9)] 
     GildedRose.new(items).update_quality()
-    expect(items[0].quality).to eq(4)
+    expect(items[0].quality).to eq(10)
   end
 
 # The Quality of an item is never more than 50
@@ -50,10 +50,10 @@ describe GildedRose do
 
     # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
   it 'Sulfuras never have to be sold or decrease quality' do
-    items = [Item.new("Sulfuras, Hand of Ragnaros", 3, 7)] 
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 10)] 
     GildedRose.new(items).update_quality()
-    expect(items[0].sell_in).to eq(3)
-    expect(items[0].quality).to eq(7)
+    expect(items[0].sell_in).to eq(10)
+    expect(items[0].quality).to eq(10)
   end
 
   end
