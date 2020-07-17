95b2055492a5098f788ce6a4cbbf9f98c8da0083
nadinedelia <nadine.delia@yahoo.com>
2020-07-17 13:03:45 +0100

    RF 4 failing tests

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -26,27 +26,22 @@ class GildedRose
       end
 
   # --------------  BACKSTAGE PASSES -------------- 
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        if item.sell_in <= DUE_DATE
-          item.quality == 0
-        elsif item.sell_in > 10 
-          item.quality += 1
-        elsif item.sell_in <= 10
-          item.quality += 2 
-        elsif item.sell_in <= 5  
-          item.quality += 3
-        else item.quality == MAX_QUALITY
-        end
-      end
+  if item.name == "Backstage passes to a TAFKAL80ETC concert"
+    item.sell_in <= 5 ? item.quality += 3 : item.quality += 2 unless item.quality == MAX_QUALITY
+   if item.sell_in == DUE_DATE
+      item.quality = MIN_QUALITY
+   end
+ end
 
-  # --------------  SULFURAS -------------- 
-      if item.name == "Sulfuras, Hand of Ragnaros"
-        item.quality == 80
+ #Sulfuras, Hand of Ragnaros
+ if item.name == "Sulfuras, Hand of Ragnaros"
+    item.quality = BEST_QUALITY
+ end
 
 
-      end
-
-
+end
+end
+end
 
 
   def basic_item(item)
@@ -55,7 +50,3 @@ class GildedRose
     item.name != "Sulfuras, Hand of Ragnaros" &&
     item.name != "Conjured"
   end
-
-end
-end
-end
